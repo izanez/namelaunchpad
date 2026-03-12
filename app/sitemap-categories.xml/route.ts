368b5450de9caa8fb7396b4dc902d0c19beadb7b
izanez <baneej.s@gmail.com>
2026-03-12 06:54:03 +0100

    Split sitemap into scalable XML indexes

---
+++ app/sitemap-categories.xml/route.ts
@@ -0,0 +1,10 @@
+import { getCategorySitemapEntries, renderUrlSet } from "@/lib/sitemaps";
+
+export function GET() {
+  return new Response(renderUrlSet(getCategorySitemapEntries()), {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, s-maxage=3600, stale-while-revalidate=86400",
+    },
+  });
+}
